536fec678dfa5ec105d6dcb3e567e40176822e06
daemonl <damien@rebase.com.au>
2013-09-10 17:52:43 +1000

    Child Errors bubble up

--- errset/user_error.go
+++ errset/user_error.go
@@ -10,11 +10,25 @@ import (
 type ErrorSet struct {
 	errors     []error
 	userErrors []string
+	parentSet  *ErrorSet
+	childSets  []*ErrorSet
 }
 
+func (s *ErrorSet) parentAppendError(err error) {
+	if s.parentSet != nil {
+		s.parentSet.AddDirect(err)
+	}
+}
+func (s *ErrorSet) parentAppendUser(err string) {
+	if s.parentSet != nil {
+		s.parentSet.AddUser(err)
+	}
+
+}
 func (s *ErrorSet) Add(err error) {
 	if err != nil {
 		s.errors = append(s.errors, err)
+		s.parentAppendError(err)
 		log.Println(err)
 	}
 }
@@ -22,23 +36,30 @@ func (s *ErrorSet) Add(err error) {
 func (s *ErrorSet) AddUserIf(err error, user string) {
 	if err != nil {
 		s.errors = append(s.errors, err)
+		s.parentAppendError(err)
 		log.Println(err)
 		s.userErrors = append(s.userErrors, user)
+		s.parentAppendUser(user)
 	}
 }
 
 func (s *ErrorSet) AddUser(user string) {
 	s.userErrors = append(s.userErrors, user)
+	s.parentAppendUser(user)
 }
 
 func (s *ErrorSet) AddUserf(format string, params ...interface{}) {
-	s.userErrors = append(s.userErrors, fmt.Sprintf(format, params...))
+	user := fmt.Sprintf(format, params...)
+	s.userErrors = append(s.userErrors, user)
+	s.parentAppendUser(user)
 }
 
 func (s *ErrorSet) AddDirect(err error) {
 	if err != nil {
 		s.userErrors = append(s.userErrors, err.Error())
+		s.parentAppendUser(err.Error())
 		s.errors = append(s.errors, err)
+		s.parentAppendError(err)
 	}
 }
 
@@ -75,7 +96,21 @@ func (e *ErrorSet) WriteJsonErrorResponse(w io.Writer) {
 	fmt.Fprintf(w, "%s", b)
 }
 
+func (e *ErrorSet) GetChildSet() *ErrorSet {
+	child := ErrorSet{
+		errors:     make([]error, 0, 0),
+		userErrors: make([]string, 0, 0),
+		parentSet:  e,
+		childSets:  make([]*ErrorSet, 0, 0),
+	}
+	return &child
+}
 func NewSet() *ErrorSet {
-	e := ErrorSet{make([]error, 0, 0), make([]string, 0, 0)}
+	e := ErrorSet{
+		errors:     make([]error, 0, 0),
+		userErrors: make([]string, 0, 0),
+		parentSet:  nil,
+		childSets:  make([]*ErrorSet, 0, 0),
+	}
 	return &e
 }
