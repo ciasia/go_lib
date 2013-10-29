09fc78394985ffb2e4c8f4e23109398e052cc4e8
daemonl <damien@rebase.com.au>
2013-10-29 13:19:25 +0000

    Databath

---
+++ databath/db.go
@@ -0,0 +1,67 @@
+package databath
+
+import (
+	"database/sql"
+)
+
+type Bath struct {
+	DriverName       string
+	ConnectionString string
+	conn             chan *Connection
+}
+
+func (bath *Bath) Connect() (*sql.DB, error) {
+	db, err := sql.Open(bath.DriverName, bath.ConnectionString)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+type Connection struct {
+	db   *sql.DB
+	bath *Bath
+}
+
+func (c *Connection) Release() {
+	c.bath.ReleaseConnection(c)
+}
+func (bath *Bath) ReleaseConnection(c *Connection) {
+	bath.conn <- c
+}
+
+func RunABath(driverName string, connectionString string, size int) *Bath {
+
+	bath := Bath{
+		DriverName:       driverName,
+		ConnectionString: connectionString,
+		conn:             make(chan *Connection, size),
+	}
+
+	for x := 0; x < size; x++ {
+		db, err := bath.Connect()
+		if err != nil {
+			panic(err)
+		}
+		conn := Connection{
+			db:   db,
+			bath: &bath,
+		}
+
+		bath.conn <- &conn
+	}
+	return &bath
+}
+
+func (bath *Bath) GetConnection() *Connection {
+	c := <-bath.conn
+	return c
+}
+
+func (c *Connection) GetDB() *sql.DB {
+	return c.db
+}
